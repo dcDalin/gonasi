99a92a4d9feb0e53a710bd2106300f8f5ea5aaad
dcDalin <30606041+dcDalin@users.noreply.github.com>
2024-06-21 13:24:01 +0300

    add types

--- app/dashboard/(tabs)/_layout.tsx
+++ app/dashboard/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from 'expo-router';
+import { Home, User } from 'lucide-react-native';
 
 export default function TabsLayout() {
   return (
@@ -7,18 +8,21 @@ export default function TabsLayout() {
         name="index"
         options={{
           title: 'Home',
-          //   tabBarIcon: ({ color }) => (
-          //     <FontAwesome size={28} name="home" color={color} />
-          //   ),
+          tabBarIcon: ({ color }) => <Home size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="courses"
+        options={{
+          title: 'Courses',
+          tabBarIcon: ({ color }) => <User size={28} color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          //   tabBarIcon: ({ color }) => (
-          //     <FontAwesome size={28} name="cog" color={color} />
-          //   ),
+          tabBarIcon: ({ color }) => <User size={28} color={color} />,
         }}
       />
     </Tabs>
---
+++ app/dashboard/(tabs)/courses.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import GoText from '@/components/GoText';
+
+export default function CoursesTabScreen() {
+  return (
+    <View>
+      <GoText text="Profile goes here" />
+    </View>
+  );
+}
